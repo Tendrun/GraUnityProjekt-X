6098d0b1757fb00762fe540251c85ce4ad63282a
Tendrun <mikisz2002@gmail.com>
2022-11-26 16:35:59 +0100

    1
    Instalacja Spring 3.0.0, JDK 17.0.5 with hotspot, GraphQL, MySQL

---
+++ projektx/src/test/java/dev/kjk_kjp/projektx/ProjektxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kjk_kjp.projektx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
